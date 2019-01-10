62cb50a6153f5e709e50b121c0f28e6680962562
pratik2799 <pithore.pratik@gmail.com>
2019-01-10 18:56:07 +0530

    added pie script
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

---
+++ Havocp.sh
@@ -0,0 +1,28 @@
+rm -rf hardware/qcom/display-caf-msm8952
+rm -rf hardware/qcom/audio-caf-msm8952
+rm -rf hardware/qcom/media-caf-msm8952
+rm -rf system/bt
+
+#tress
+
+git clone https://github.com/Pratik2799/android_device_xiaomi_msm8956-common -b Havoc-p device/xiaomi/msm8956-common
+
+git clone https://github.com/Pratik2799/android_device_xiaomi_kenzo -b Havoc-p device/xiaomi/kenzo
+
+git clone https://github.com/AmolAmrit/Escrima_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956
+
+git clone https://github.com/Pratik2799/android_vendor_xiaomi_kenzo -b arrow-9.x vendor/xiaomi
+
+#HALs
+
+git clone https://github.com/Pratik2799/android_hardware_qcom_media -b pie hardware/qcom/media-caf-msm8952
+
+git clone https://github.com/Pratik2799/android_hardware_qcom_audio -b pie hardware/qcom/audio-caf-msm8952
+
+git clone https://github.com/Pratik2799/android_hardware_qcom_display -b pie hardware/qcom/display-caf-msm8952
+
+#Extra
+
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 packages/resources/devicesettings
+
+git clone https://github.com/Pratik2799/android_system_bt -b pie system/bt
