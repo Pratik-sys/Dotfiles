0657771c5c5ef869d8dbba35f6f93be7025a1b96
pratik2799 <pithore.pratik@gmail.com>
2019-01-21 22:19:08 +0530

    test with new trees
    *testing with rebased trees.
    
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

--- Havocp.sh
+++ Havocp.sh
@@ -5,13 +5,13 @@ rm -rf system/bt
 
 #tress
 
-git clone https://github.com/Pratik2799/android_device_xiaomi_msm8956-common -b Havoc-p device/xiaomi/msm8956-common
+git clone https://github.com/Pratik2799/android_device_xiaomi_msm8956-common-1 -b pie device/xiaomi/msm8956-common
 
-git clone https://github.com/Pratik2799/android_device_xiaomi_kenzo -b Havoc-p device/xiaomi/kenzo
+git clone https://github.com/Pratik2799/android_device_xiaomi_kenzo-1 -b pie device/xiaomi/kenzo
 
 git clone https://github.com/AmolAmrit/Escrima_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956
 
-git clone https://github.com/Pratik2799/android_vendor_xiaomi_kenzo -b arrow-9.x vendor/xiaomi
+git https://github.com/Pratik2799/proprietary_vendor_xiaomi-1 -b pie vendor/xiaomi
 
 #HALs
 
@@ -24,3 +24,4 @@ git clone https://github.com/Pratik2799/android_hardware_qcom_display -b pie har
 #Extra
 
 git clone https://github.com/AospExtended/platform_system_bt -b 9.x system/bt
+
