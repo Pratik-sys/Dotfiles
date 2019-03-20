43f526beca2390347a7d82932b5d6d32fd0e219e
pratik2799 <pithore.pratik@gmail.com>
2019-03-20 19:00:12 +0530

    update script

--- Havocp.sh
+++ Havocp.sh
@@ -1,8 +1,6 @@
 rm -rf hardware/qcom/display-caf-msm8952
 rm -rf hardware/qcom/audio-caf-msm8952
 rm -rf hardware/qcom/media-caf-msm8952
-rm -rf system/bt
-rm -rf packages/apps/HavocSettings
 
 #tress
 
@@ -10,22 +8,18 @@ git clone https://github.com/Pratik2799/android_device_xiaomi_msm8956-common-1 -
 
 git clone https://github.com/Pratik2799/android_device_xiaomi_kenzo-1 -b pie device/xiaomi/kenzo
 
-git clone https://github.com/Pratik2799/Escrima_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956
+git clone https://github.com/AmolAmrit/Escrima_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956
 
 git clone https://github.com/AmolAmrit/proprietary_vendor_xiaomi -b pie vendor/xiaomi
 
 #HALs
 
-git clone https://github.com/Pratik2799/android_hardware_qcom_media -b pie hardware/qcom/media-caf-msm8952
+git clone https://github.com/AmolAmrit/android_hardware_qcom_media -b pie hardware/qcom/media-caf-msm8952
 
-git clone https://github.com/Pratik2799/android_hardware_qcom_audio -b pie hardware/qcom/audio-caf-msm8952
+git clone https://github.com/AmolAmrit/android_hardware_qcom_audio -b pie hardware/qcom/audio-caf-msm8952
 
-git clone https://github.com/Pratik2799/android_hardware_qcom_display -b pie hardware/qcom/display-caf-msm8952
+git clone https://github.com/AmolAmrit/android_hardware_qcom_display -b pie hardware/qcom/display-caf-msm8952
 
 #Extra
-
-git clone https://github.com/Pratik2799/platform_system_bt -b pie system/bt
-
-git clone https://github.com/Pratik2799/android_packages_apps_HavocSettings -b p1 packages/apps/HavocSettings
 
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 packages/resources/devicesettings
