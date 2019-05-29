43cbd6901fa4de0629fc3d933c384b75eca080b0
Pratik Pithore <pithore.pratik@gmail.com>
2019-05-29 08:26:18 +0530

    changed the kernel path and branch for packages

--- Havocp.sh
+++ Havocp.sh
@@ -10,7 +10,7 @@ git clone https://github.com/Pratik2799/android_device_xiaomi_msm8956-common-1 -
 
 git clone https://github.com/Pratik2799/android_device_xiaomi_kenzo-1 -b pie device/xiaomi/kenzo
 
-git clone https://github.com/aranhid/android_kernel_xiaomi_msm8956 -b nos-pie kernel/xiaomi/msm8956
+git clone https://github.com/AmolAmrit/Escrima_kernel_xiaomi_msm8956 -b pie kernel/xiaomi/msm8956
 
 git clone https://github.com/AmolAmrit/proprietary_vendor_xiaomi -b pie vendor/xiaomi
 
@@ -26,4 +26,4 @@ git clone https://github.com/AmolAmrit/android_hardware_qcom_display -b pie hard
 
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 packages/resources/devicesettings
 
-git clone https://github.com/Pratik2799/android_packages_apps_HavocSettings -b p1 packages/apps/HavocSettings
+git clone https://github.com/Pratik2799/android_packages_apps_HavocSettings -b p2 packages/apps/HavocSettings
